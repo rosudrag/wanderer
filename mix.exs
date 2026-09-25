c3167f27a4622d4d3cebb9cc6cff64185f46041e
Chewy <rosudrag.dev@gmail.com>
2026-09-25 21:00:35 +0200

    chewy: bump version for layout stability work

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule WandererApp.MixProject do
 
   @source_url "https://github.com/wanderer-industries/wanderer"
 
-  @version "1.103.4-chewy.9"
+  @version "1.103.4-chewy.10"
 
   def project do
     [
